bcc3a770f40028a7ab30498c5d632be04f2838cb
FloParret <flo.parret@gmail.com>
2020-08-04 11:03:37 +0200

    fix merge conflicts

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { PrivateRoute } from "../helpers/PrivateRoute";
-import Login from "./login/Login";
 
 import AccueilSocial from "./social/SocialHome";
 import ListEmployes from "./social/gestion_employes/List_employes";
@@ -12,13 +11,26 @@ import Contrat from "./contrat/Contrat";
 import DetailContrat from "./contrat/detailContrat/DetailContrat";
 import CreerContrat from "./contrat/creerContrat/CreerContrat";
 
-import MenuClientFournisseur from "./component-clientFournisseur/menuClientFournisseur";
+import MenuClientFournisseur from "./clientFournisseur/menuClientFournisseur";
 
 import Bancaire from "./bancaire";
 import CreationOperation from "./bancaire/operation/creationOperation/creationOperation";
 import IndexOperation from "./bancaire/operation/index";
 import ListeOperations from "./bancaire/operation/listeOperations/listeOperations";
-import RoutesWithNavigation from "./RoutesWithNavigation";
+// gestionUserRoutesImports
+import Users from "./users/Users";
+import NotFound from "./pages/NotFound";
+import EditUser from "./users/EditUser";
+import AddUser from "./users/AddUser";
+import ViewUser from "./users/ViewUser";
+import AddComptable from "./comptables/AddComptable";
+import ViewComptable from "./comptables/ViewComptable";
+import EditComptable from "./comptables/EditComptable";
+import SelectToAddUser from "./pages/SelectToAddUser";
+import AddSociete from "./societe/AddSociete";
+import ViewSociete from "./societe/ViewSociete";
+import EditSociete from "./societe/EditSociete";
+// finGestionUserRoutesImports
 
 export default class Routes extends Component {
   render() {
@@ -32,7 +44,28 @@ export default class Routes extends Component {
           <PrivateRoute path="/contrat" component={Contrat} />
           <PrivateRoute path="/detailcontrat/:id" component={DetailContrat} />
           <PrivateRoute path="/creercontrat" component={CreerContrat} />
+          {/* gestionUserRoutes */}
+          <Route exact path="/users" component={Users} />
+          <Route exact path="/users/add" component={AddUser} />
+          <Route exact path="/users/edit/:id" component={EditUser} />
+          <Route exact path="/users/view/:id" component={ViewUser} />
+          <Route exact path="/users/add/comptable" component={AddComptable} />
+          <Route
+            exact
+            path="/users/view/comptable/:id"
+            component={ViewComptable}
+          />
+          <Route
+            exact
+            path="/users/edit/comptable/:id"
+            component={EditComptable}
+          />
+          <Route exact path="/users/add/societe" component={AddSociete} />
+          <Route exact path="/users/view/societe/:id" component={ViewSociete} />
+          <Route exact path="/users/edit/societe/:id" component={EditSociete} />
+          <Route exact path="/users/stau" component={SelectToAddUser} />
 
+          {/* finGestionUserRoutes */}
           <PrivateRoute path="/bancaire" component={Bancaire} />
           <PrivateRoute
             path="/creationoperation"
@@ -42,6 +75,7 @@ export default class Routes extends Component {
           <PrivateRoute path="/listeoperations" component={ListeOperations} />
 
           <Route path="/client-fournisseur" component={MenuClientFournisseur} />
+          <Route component={NotFound} />
         </Switch>
       </BrowserRouter>
     );
