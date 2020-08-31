43a74a246b35caf88bd04760ff0105c7903343f6
rob Marley <robin2ruyck@gmail.com>
2020-08-31 11:40:52 +0200

    #1143 RD - Add operation edition

---
+++ src/components/bancaire/releve/details_releve/operation/edit_operation/editOperation.js
@@ -0,0 +1,231 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import AxiosCenter from "../../../../../../shared/services/AxiosCenter";
+import Loading from "../../../../../../shared/component/Loading";
+import UserService from "../../../../../../shared/services/UserService";
+import
+{
+  MDBContainer,
+  MDBBtn,
+  MDBRow,
+  MDBCardBody,
+  MDBCard,
+  MDBInput,
+  MDBCol,
+} from "mdbreact";
+
+const lexique =
+{
+  requiredDesc  : "*Descritpion obligatoire",
+  requiredDate  : "*Date obligatoire",
+  requiredType  : "*Type obligatoire",
+  requiredSolde : "*Solde obligatoire et non négatif",
+  requireMention: "* Mention obligatoire requis",
+  title         : "Modification d'opération",
+  operationType : { name: "Type d'operation*", debit: "Débit", credit: "Crédit"},
+  date          : "Date*",
+  description   : "Description*",
+  solde         : "Solde (€)*"
+}                                                                                                                                                                                                                                                
+
+const operationSchema = Yup.object().shape(
+{
+  date          : Yup.date().required(lexique.requiredDate).max(new Date()),
+  description   : Yup.string().required(lexique.requiredDesc),
+  type          : Yup.string().required(lexique.requiredType),
+  solde         : Yup.number().required(lexique.requiredSolde).min(0, lexique.requiredSolde)
+});
+
+const ComposantErreur = (props) => (
+  <div className="text-danger">{props.children}</div>
+);
+
+const ComposantInput = ({ field, form: { touched, errors }, ...props }) => (
+  <MDBInput label={props.label} outline type="text" {...props} {...field} />
+);
+
+const ComposantDate = ({ field, form: { touched, errors }, ...props }) => (
+  <MDBInput label={props.label} outline type="date" max={new Date().toISOString().split("T")[0]} {...props} {...field} />
+);
+
+class EditOperation extends React.Component {
+    constructor(props) {
+    super(props);
+    this.state = {
+      loaded: false,
+      operation: {},
+      roleUser: UserService.getRole()
+    };
+  }
+
+  getInitialValues = () => {
+    return this.state.operation;
+  };
+
+  isTypeSelected = (value) => {
+    if (value == this.state.type) return "selected";
+  }
+
+  componentDidMount() {
+    if (this.state.roleUser === "ROLE_SOCIETY") {
+      const idOperation = this.props.match.params.id;
+      AxiosCenter.getOperationById(idOperation)
+      .then((response) => {
+        const operation = response.data;
+        this.setState({ operation: operation, loaded: true });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+  }
+
+  submit = (values, actions) => {
+    AxiosCenter.updateOperation(values)
+      .then((response) => {
+        const operation = response.data;
+        this.props.history.push("/detailsreleveinvalide/" + operation.releveId);
+      })
+      .catch((err) => console.log(err));
+    actions.setSubmitting(true);
+  };
+
+  render() {
+    if (!this.state.loaded) return <Loading />;
+    return (
+      <div className="container-fluid p-5 bg-light d-flex flex-column justify-content-center align-items-center">
+        <MDBContainer>
+          <h1>{lexique.title}</h1>
+        <Formik
+              onSubmit={this.submit}
+              initialValues={this.getInitialValues()}
+              validationSchema={operationSchema}
+              enableReinitialize={true}
+            >
+              {({
+                values,
+                errors,
+                touched,
+                dirty,
+                isSubmitting,
+                handleSubmit,
+                handleChange,
+                handleBlur,
+                handleReset,
+              }) => (
+                <Form onSubmit={handleSubmit}>
+                  <div>
+                    <MDBCardBody>
+                      <MDBCard className="cadre">
+                        <MDBRow around>
+                          <MDBCol md="6" className="mb-3">
+                            {/* Date  condition : verif date supérieur à date ajd */}
+                            <Field
+                              name="date"
+                              label={lexique.date}
+                              value={values.date}
+                              component={ComposantDate}
+                            />
+                            <ErrorMessage
+                              name="date"
+                              component={ComposantErreur}
+
+                            />
+
+                            {/* Description */}
+                            <div>
+                              <Field
+                                name="description"
+                                label={lexique.description}
+                                component={ComposantInput}
+                              />
+                              <ErrorMessage
+                                name="description"
+                                component={ComposantErreur}
+                              />
+                            </div>
+
+                            {/* Type */}
+                            <div>
+                              <select
+                                className="browser-default custom-select"
+                                name="type"
+                                onChange={handleChange}
+                                value={values.type}
+                                onBlur={handleBlur}
+                              >
+                                <option value="debit">{lexique.operationType.debit}</option>
+                                <option value="credit">{lexique.operationType.credit}</option>
+                              </select>
+                              {errors.type && touched.type && (
+                                <div className="text-danger">
+                                  {errors.type}
+                                </div>
+                              )}
+                            </div>
+
+                            { /* Solde */ }
+                            <div>
+                              <Field
+                                name="solde"
+                                label={lexique.solde}
+                                component={ComposantInput}
+                              />
+                             <ErrorMessage
+                              name="solde"
+                              component={ComposantErreur}
+                            />
+                            </div>
+                          </MDBCol>
+                        </MDBRow>
+                      </MDBCard>
+                    </MDBCardBody>
+                  </div>
+                  <p className="small">* Mention Obligatoire</p>
+                  <div>
+                    <hr></hr>
+                  </div>
+                  <div>
+                    <MDBBtn
+                      color="teal accent-3"
+                      rounded
+                      size="sm"
+                      type="submit"
+                    >
+                      Enregistrer
+                    </MDBBtn>
+
+                    <MDBBtn
+                      color="teal accent-3"
+                      rounded
+                      size="sm"
+                      type="reset"
+                      onClick={handleReset}
+                      disabled={!dirty || isSubmitting}
+                    >
+                      RESET
+                    </MDBBtn>
+
+                    <MDBBtn
+                      color="teal accent-3"
+                      rounded
+                      size="sm"
+                      onClick={() => {
+                        this.props.history.push(
+                          "/detailsreleveinvalide/" + this.state.operation.releveId
+                        );
+                      }}
+                    >
+                      Retour
+                    </MDBBtn>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+        </MDBContainer>
+      </div>
+    );
+  }
+}
+export default EditOperation;
