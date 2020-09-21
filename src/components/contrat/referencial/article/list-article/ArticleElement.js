f79fdf9896bf68151b02d882d3ad2d3102881df7
eddy62 <eddyszandzik@gmail.com>
2020-09-21 10:10:23 +0200

    [#860860] [fix] [ES] - delete role admin

--- src/components/contrat/referencial/article/list-article/ArticleElement.js
+++ src/components/contrat/referencial/article/list-article/ArticleElement.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 
 export default class ArticleElement extends Component {
+
     render() {
         return (
             <tr>
-                <td>{this.props.article.id}</td>
-                <td>{this.props.article.reference}</td>
                 <td>{this.props.article.titre}</td>
+                <td>{this.props.article.intitule}</td>
                 <td>{this.props.article.description}</td>
             </tr>
         );
