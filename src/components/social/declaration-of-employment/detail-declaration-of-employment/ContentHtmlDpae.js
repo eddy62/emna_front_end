8950e43239d09bfc9991c4da79e5fa34499a2f83
Erik DUNAIS <erik.dunais@free.fr>
2020-10-22 08:36:16 +0200

    [1355][feature][ED] - Detail of a Dpae (html)

--- src/components/social/declaration-of-employment/detail-declaration-of-employment/ContentHtmlDpae.js
+++ src/components/social/declaration-of-employment/detail-declaration-of-employment/ContentHtmlDpae.js
@@ -24,24 +24,16 @@ class ContentHtmlDpae extends React.Component{
     }
 
     componentDidMount() {
-        
+        // TODO : recevoir id depuis ConsultDeclarationOfEmployement
         AxiosCenter.getHtmlDpae(1)
-       
             .then((response) => {
                 this.setState({codeHtml: response.data.html})
-        console.log(this.state.codeHtml)
-
-        
-
             })
     }
 
     render() {
         return (
             <div className="content" dangerouslySetInnerHTML={{ __html: this.state.codeHtml }}></div>
-                // var code  = this.state.codeHtml;
-                // var parse = require('html-react-parser');
-                // parse('<div>code</div>');
         );
     }
 }
